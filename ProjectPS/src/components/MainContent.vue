1a772e945d814648f93440e3e4a110f54ebb0c22
minwoo <128667859+mainu0221@users.noreply.github.com>
2025-11-24 18:59:06 +0900

    v0.2.5

--- ProjectPS/src/components/MainContent.vue
+++ ProjectPS/src/components/MainContent.vue
@@ -50,7 +50,7 @@
 
         <button
           @click="moveTo('skill-section')"
-          class="px-8 py-4 bg-white border-2 border-gray-300 text-gray-700 rounded-full font-bold text-lg hover:border-emerald-500 hover:text-emerald-500 transition shadow-lg transform hover:-translate-y-1"
+          class="px-8 py-4 bg-white border-2 border-gray-800 text-gray-800 rounded-full font-bold text-lg hover:border-emerald-500 hover:text-emerald-500 transition shadow-lg transform hover:-translate-y-1"
         >
           Main Skills
         </button>
@@ -67,7 +67,7 @@
           <a
             href="/PortfolioSite/cv.pdf"
             target="_blank"
-            class="w-44 flex items-center justify-center gap-2 px-5 py-2 bg-white border-2 border-gray-800 text-gray-800 rounded-lg hover:bg-gray-100 hover:-translate-y-1 transition shadow-md font-bold text-sm"
+            class="w-44 flex items-center justify-center gap-2 px-5 py-2 bg-white border-2 border-gray-800 text-gray-800 rounded-lg hover:border-emerald-500 hover:text-emerald-500 hover:-translate-y-1 transition shadow-md font-bold text-sm"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
